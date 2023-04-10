5a41058dd5577749b4308d7becd9a8f754670699
jegerud <kristjeg@stud.ntnu.no>
2023-04-10 10:55:17 +0200

    Mid step

--- src/data_utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# from .mnist import MNISTDetectionDataset
-# from .voc import VOCDataset
-from .road_damage import RoadDamageDataset
--- src/data_utils/transforms/__init__.py
+++
@@ -1,4 +0,0 @@
-from .transform import  ToTensor, RandomSampleCrop, RandomHorizontalFlip, Resize
-from .target_transform import GroundTruthBoxesToAnchors
-from .anchor_encoder import AnchorEncoder
-from .gpu_transforms import Normalize, ColorJitter
