5f829b483ce705f1d74e305d41b14c4698cd3883
jegerud <kristjeg@stud.ntnu.no>
2023-04-10 10:51:38 +0200

    stripped down and simplified version without lazy loading

--- src/data_exploration.py
+++ src/data_exploration.py
@@ -2,32 +2,26 @@ import sys
 assert sys.version_info >= (3, 7), "This code requires python version >= 3.7"
 import click
 import torch
-import tops
-from sklearn.model_selection import train_test_split
-from pathlib import Path
-from ssd import utils
-from ssd.data.utils.preprocess import get_mean_box_area, get_number_of_boxes, get_label_mean_center
-from ssd.data.utils.generate_train_val import generate_train_val
-from tops.config import instantiate
+from data_utils.utils.preprocess import get_mean_box_area, get_number_of_boxes, get_label_mean_center
+from data_utils.utils.generate_train_val import generate_train_val
+# from config.resnet import data_train, data_val, train_set_dir, batch_size
+from config.resnet import Dataset
+from data_utils import RoadDamageDataset
+from config.utils import get_dataset_dir
+
 torch.backends.cudnn.benchmark = True
 
+def data_exploration():
+    generate_train_val()
 
-@click.command()
-@click.argument("config_path", type=click.Path(exists=True, dir_okay=False, path_type=Path))
-def load_data(config_path: Path):
-    cfg = utils.load_config(config_path)
-    generate_train_val(cfg=cfg)
+    dataset = Dataset()
+    dataset_train = dataset.dataset_train
+    dataset_val = dataset.dataset_val
+    dataloader_train = dataset.dataloader_train
+    dataloader_val = dataset.dataloader_val
 
-    tops.init(cfg.output_dir)
-    tops.set_AMP(cfg.train.amp)
-    tops.set_seed(cfg.train.seed)
-    dataloader_train = instantiate(cfg.data_train.dataloader)
-    dataloader_val = instantiate(cfg.data_val.dataloader)
-    
-    # train_cocoGt = dataloader_train.dataset.get_annotations_as_coco()
-    # val_cocoGt = dataloader_val.dataset.get_annotations_as_coco()
-
-    subsamples_train = (dataloader_train.dataset.image_ids)
+    # print(f"Data_train: {len(dataloader_train)}")
+    subsamples_train = (dataset_train.image_ids)
     means_train = get_mean_box_area(dataloader=dataloader_train, subsamples=subsamples_train)
     counts_train = get_number_of_boxes(dataloader=dataloader_train, subsamples=subsamples_train)
     center_train = get_label_mean_center(dataloader=dataloader_train, subsamples=subsamples_train)
@@ -39,19 +33,5 @@ def load_data(config_path: Path):
         print(f"  Mean size: {means_train[key]}")
         print(f"  Mean center: {center_train[key]}")
 
-
-    subsamples_val = (dataloader_val.dataset.image_ids)
-    means_val = get_mean_box_area(dataloader=dataloader_val, subsamples=subsamples_val)
-    counts_val = get_number_of_boxes(dataloader=dataloader_val, subsamples=subsamples_val)
-    center_val = get_label_mean_center(dataloader=dataloader_val, subsamples=subsamples_val)
-    
-    print(f"\n\nVALIDATION: {len(subsamples_val)} samples")
-    for key in means_val:
-        print(f"\nLabel: {key}")
-        print(f"  Counts in set: {counts_val[key]}")
-        print(f"  Mean size: {means_val[key]}")
-        print(f"  Mean center: {center_val[key]}")
-
-
 if __name__ == "__main__":
-    load_data()
+    data_exploration()
--- src/train.py
+++ src/train.py
@@ -1,124 +1,137 @@
 import sys
-assert sys.version_info >= (3, 7), "This code requires python version >= 3.7"
+# assert sys.version_info >= (3, 7), "This code requires python version >= 3.7"
 import functools
 import time
+from timeit import default_timer as timer 
 import click
 import torch
-from omegaconf import OmegaConf
-import pprint
-import tops
+import torchvision
+# from tqdm import tqdm
 import tqdm
-from pathlib import Path
-from ssd.evaluate import evaluate
-from ssd import utils
-from tops.config import instantiate
-from tops import logger, checkpointer
-from torch.optim.lr_scheduler import ChainedScheduler
-torch.backends.cudnn.benchmark = True
-from ssd.data.utils.generate_train_val import generate_train_val
+from torchvision import models
+from data_utils.utils.generate_train_val import generate_train_val
+from config.resnet import Dataset, num_classes, gpu_transform, epochs
+from utils.torch_utils import to_cuda
+from modelling import ResNet
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_V2_Weights
 
+# def train_step(model: torch.nn.Module, 
+#                dataloader: torch.utils.data.DataLoader,
+#                optimizer: torch.optim.Optimizer,
+#                epoch: int):
+    
+#     model.train()
+#     losses =  0
+
+#     # for batch, (X, y, filename) in tqdm(enumerate(dataloader)):
+
+#     # idx = 0
+#     for batch in enumerate(tqdm.tqdm(dataloader, f"Epoch {epoch}")):
+#         print(f"Batch {batch}\n\n")
+        
+#         # print(f"Targets: {targets}")
+#         # images = to_cuda(images)
+#         # targets = to_cuda(targets)
+
+#         # loss_dict = model(images, targets)
+#         # losses = sum(loss for loss in loss_dict.values())
+#         # loss_value = losses.item()
+#         # losses.backward()
+#         # optimizer.step()
+
+#     print(f"Losses: {losses}")
+
+#     return losses
+
+    
 def train_epoch(
-        model, scaler: torch.cuda.amp.GradScaler,
-        optim, dataloader_train, scheduler,
-        gpu_transform: torch.nn.Module,
-        log_interval: int):
-    grad_scale = scaler.get_scale()
-    for batch in tqdm.tqdm(dataloader_train, f"Epoch {logger.epoch()}"):
-        batch = tops.to_cuda(batch)
-        batch["labels"] = batch["labels"].long()
-        batch = gpu_transform(batch)
+        model, 
+        dataloader,
+        optimizer,
+        epoch
+    ):
+    # losses = {
+    #     "train_loss": [],
+    #     "test_loss": [],
+    # }
+    model.train()
 
-        with torch.cuda.amp.autocast(enabled=tops.AMP()):
-            bbox_delta, confs = model(batch["image"])
-            loss, to_log = model.loss_func(bbox_delta, confs, batch["boxes"], batch["labels"])
-        scaler.scale(loss).backward()
-        scaler.step(optim)
-        scaler.update()
-        optim.zero_grad()
-        if grad_scale == scaler.get_scale():
-            scheduler.step()
-            if logger.global_step() % log_interval:
-                logger.add_scalar("stats/learning_rate", scheduler._schedulers[-1].get_last_lr()[-1])
-        else:
-            grad_scale = scaler.get_scale()
-            logger.add_scalar("amp/grad_scale", scaler.get_scale())
-        if logger.global_step() % log_interval == 0:
-            to_log = {f"loss/{k}": v.mean().cpu().item() for k, v in to_log.items()}
-            logger.add_dict(to_log)
-        # torch.cuda.amp skips gradient steps if backward pass produces NaNs/infs.
-        # If it happens in the first iteration, scheduler.step() will throw exception
-        logger.step()
+    test_loss = 0
+    train_loss= 0
 
-    return
+    for X, y in tqdm.tqdm(dataloader, f"Epoch {epoch}"):
+        optimizer.zero_grad()
+        # batch = to_cuda(batch)
+        # print(f"X: {X}")
+        # print(f"y: {y}")
+        X = to_cuda(X)
+        # y = to_cuda(y)
+        # batch = gpu_transform(batch)
+        print("Before model(X, y)")
+
+        with torch.set_grad_enabled(True):
+            output = model(X, y)
+            
+            loss = sum(loss for loss in output.values())
+            train_loss += loss.item()
+            # loss.backward()
+            # optimizer.step()
+        
+        # if batch % 200 == 0:
+        #     print(f"Iteration #{batch} loss: {loss}")
+
+        # Print out what's happening
+        print(f"Epoch: {epoch+1} | train_loss: {train_loss} | test_loss: {test_loss}")
+
+    #     losses["train_loss"].append(train_loss)
+    #     # losses["test_loss"].append(test_loss)
+    train_loss = train_loss / len(dataloader)
+
+    return train_loss
 
 
-def print_config(cfg):
-    container = OmegaConf.to_container(cfg)
-    pp = pprint.PrettyPrinter(indent=2, compact=False)
-    print("--------------------Config file below--------------------")
-    pp.pprint(container)
-    print("--------------------End of config file--------------------")
+def train():
+    generate_train_val()
 
+    dataset = Dataset()
+    dataset_train = dataset.dataset_train
+    dataset_val = dataset.dataset_val
+    dataloader_train = dataset.dataloader_train
+    dataloader_val = dataset.dataloader_val
 
-@click.command()
-@click.argument("config_path", type=click.Path(exists=True, dir_okay=False, path_type=Path))
-@click.option("--evaluate-only", default=False, is_flag=True, help="Only run evaluation, no training.")
-def train(config_path: Path, evaluate_only: bool):
-    logger.logger.DEFAULT_SCALAR_LEVEL = logger.logger.DEBUG
-    cfg = utils.load_config(config_path)
-    print_config(cfg)
+    # print(f"Dataloader train: {dataloader_train}")
+    # print(f"Len dataloader training: {len(dataloader_train)}")
 
-    generate_train_val(cfg=cfg)
+    model_obj = ResNet()
+    model = model_obj.model
 
-    tops.init(cfg.output_dir)
-    tops.set_AMP(cfg.train.amp)
-    tops.set_seed(cfg.train.seed)
-    dataloader_train = instantiate(cfg.data_train.dataloader)
-    dataloader_val = instantiate(cfg.data_val.dataloader)
-    cocoGt = dataloader_val.dataset.get_annotations_as_coco()
-    model = tops.to_cuda(instantiate(cfg.model))
-    optimizer = instantiate(cfg.optimizer, params=utils.tencent_trick(model))
-    scheduler = ChainedScheduler(instantiate(list(cfg.schedulers.values()), optimizer=optimizer))
-    checkpointer.register_models(
-        dict(model=model, optimizer=optimizer, scheduler=scheduler))
-    total_time = 0
-    if checkpointer.has_checkpoint():
-        train_state = checkpointer.load_registered_models(load_best=False)
-        total_time = train_state["total_time"]
-        logger.log(f"Resuming train from: epoch: {logger.epoch()}, global step: {logger.global_step()}")
+    # print(type(model))
 
-    gpu_transform_val = instantiate(cfg.data_val.gpu_transform)
-    gpu_transform_train = instantiate(cfg.data_train.gpu_transform)
-    evaluation_fn = functools.partial(
-        evaluate,
-        model=model,
-        dataloader=dataloader_val,
-        cocoGt=cocoGt,
-        gpu_transform=gpu_transform_val,
-        label_map=cfg.label_map
-    )
-    if evaluate_only:
-        evaluation_fn()
-        exit()
-    scaler = torch.cuda.amp.GradScaler(enabled=tops.AMP())
-    dummy_input = tops.to_cuda(torch.randn(1, cfg.train.image_channels, *cfg.train.imshape))
-    tops.print_module_summary(model, (dummy_input,))
-    start_epoch = logger.epoch()
-    for epoch in range(start_epoch, cfg.train.epochs):
+    num_features = model.roi_heads.box_predictor.bbox_pred.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(num_features, num_classes)
+    print(f"Model:\n{model}")
+    
+    model = to_cuda(model)
+
+    torch.manual_seed(42)
+    torch.cuda.manual_seed(42)
+
+    # start_time = timer()
+    for epoch in range(0, epochs):
         start_epoch_time = time.time()
-        train_epoch(model, scaler, optimizer, dataloader_train, scheduler, gpu_transform_train, cfg.train.log_interval)
+        results = train_epoch(
+            model=model,
+            dataloader=dataloader_train,
+            optimizer=model_obj.optimizer,
+            epoch=epoch
+        )
         end_epoch_time = time.time() - start_epoch_time
         total_time += end_epoch_time
-        logger.add_scalar("stats/epoch_time", end_epoch_time)
 
-        eval_stats = evaluation_fn()
-        eval_stats = {f"metrics/{key}": val for key, val in eval_stats.items()}
-        logger.add_dict(eval_stats, level=logger.logger.INFO)
-        train_state = dict(total_time=total_time)
-        checkpointer.save_registered_models(train_state)
-        logger.step_epoch()
-    logger.add_scalar("stats/total_time", total_time)
-
+    # end_time = timer()
+    # print(f"[INFO] Total training time: {end_time-start_time:.3f} seconds")
+    # End the timer and print out how long it took
 
 if __name__ == "__main__":
     train()
