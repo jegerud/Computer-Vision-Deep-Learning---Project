2bd4788e248337ba0eb9b2d7d3466ae63798feb6
jegerud <kristjeg@stud.ntnu.no>
2023-04-18 13:14:21 +0200

    Finally a model that can be trained with some decent results

--- src/utils/torch_utils.py
+++ src/utils/torch_utils.py
@@ -23,24 +23,6 @@ def to_cuda(elements):
         return {k: _to_cuda(v) for k,v in elements.items()}
     return _to_cuda(elements)
 
-def move_to(obj):
-    device = get_device()
-    # print(f"Object type: {type(obj)}")
-    # Recursively move the elements of an object to device, used for annotations
-    if torch.is_tensor(obj): 
-        return obj.to(device)
-    elif isinstance(obj, dict):
-        res = {}
-        for k, v in obj.items(): res[k] = move_to(v)
-        return res
-    elif isinstance(obj, list):
-        res = []
-        for v in obj: res.append(move_to(v))
-        return res
-    else:
-        print(f"Type obj: {type(obj)}")
-        raise TypeError("Invalid type for move_to")
-
 
 def get_device() -> torch.device:
     return torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
